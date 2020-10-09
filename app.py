426e7e873807c2ba4fb61df33929ffb255188bd3
Irushan_IT17121866 <it17121866@my.sliit.lk>
2020-10-09 23:30:30 +0530

    CORS added

--- app.py
+++ app.py
@@ -1,36 +1,71 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 book_list = [
     {
-        "id":0,
-        "title":"sun of rise",
+        "updates": [
+            {
+                "recNo": 1,
+                "time": 0,
+                "forefoot_strike": 234,
+                "midfoot_strike": 2,
+                "rarefoot_strike": 2
+            },
+            {
+                "recNo": 2,
+                "time": 2,
+                "forefoot_strike": 322,
+                "midfoot_strike": 2,
+                "rarefoot_strike": 2
+            }
+        ],
+        "sensorName": "Smart_shoe_left"
     },
-     {
-        "id":1,
-        "title":"game of throns",
+    {
+        "updates": [
+            {
+                "recNo": 1,
+                "time": 0,
+                "forefoot_strike": 800,
+                "midfoot_strike": 2,
+                "rarefoot_strike": 2
+            },
+            {
+                "recNo": 2,
+                "time": 2,
+                "forefoot_strike": 900,
+                "midfoot_strike": 2,
+                "rarefoot_strike": 2
+            }
+        ],
+        "sensorName": "Smart_shoe_left"
     },
 ]
 
-@app.route('/books',methods=['GET','POST'])
+
+@app.route('/books', methods=['GET', 'POST'])
 def books():
     if request.method == 'GET':
-        if len(book_list)>0:
-            return jsonify(book_list)
+        if len(book_list) > 0:
+            response = flask.jsonify(book_list)
+            response.headers.add('Access-Control-Allow-Origin', '*')
         else:
             'Nothing Found', 404
 
-    if request.method == 'POST':    
+    if request.method == 'POST':
         new_title = request.form['title']
         iD = book_list[-1]['id']+1
 
         new_obj = {
-            'id':iD,
-            'title':new_title
+            'id': iD,
+            'title': new_title
         }
         book_list.append(new_obj)
         return jsonify(book_list), 201
+
 
 if __name__ == '__main__':
     app.run(debug=True)
